7ad4cf97414683eb874b243fd0ea10eaddf05628
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2020-05-24 17:55:20 -0400

    primeros comentarios

---
+++ sintaxis.dart
@@ -0,0 +1,3 @@
+//este documento tendra la sintaxis basica del lenguaje dart//
+//se estima que el lector tiene una nocion basica del lenguaje//
+//por lo que este fragmento de codigo solo sera un vistazo rapido//
